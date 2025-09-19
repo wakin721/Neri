e57e8eb3e780312c909d7c2b38f45646e8ea3022
和錦わきん <156275155+wakin721@users.noreply.github.com>
2025-09-19 21:41:20 +0800

    Update config.py

--- system/config.py
+++ system/config.py
@@ -3,8 +3,8 @@
 """
 
 # 应用信息常量
-APP_TITLE = "Neri v2.0.5"
-APP_VERSION = "2.0.5-beta"
+APP_TITLE = "Neri v2.0.6"
+APP_VERSION = "2.0.6-beta"
 DEFAULT_EXCEL_FILENAME = "物种检测信息.xlsx"
 
 # 文件支持相关常量
@@ -18,6 +18,7 @@ BUTTON_WIDTH = 14
 LARGE_FONT = ('Segoe UI', 11)
 NORMAL_FONT = ('Segoe UI', 10)
 SMALL_FONT = ('Segoe UI', 9)
+
 
 
 
