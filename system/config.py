24ce3ceeb4fb9c9536dcfb0ce2cc3670d23f8a68
和錦わきん <156275155+wakin721@users.noreply.github.com>
2025-12-25 00:50:23 +0800

    Add files via upload

--- system/config.py
+++ system/config.py
@@ -4,8 +4,8 @@
 import zlib  # Add this import for stable hashing
 
 # 应用信息常量
-APP_TITLE = "Neri v2.2.5"
-APP_VERSION = "2.2.5-beta"
+APP_TITLE = "Neri v2.2.6"
+APP_VERSION = "2.2.6-beta"
 DEFAULT_EXCEL_FILENAME = "物种检测信息.xlsx"
 
 # 文件支持相关常量
