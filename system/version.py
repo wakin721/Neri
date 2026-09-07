844bd3557d5a306f0b692532ab4a29ca21a6196e
和錦わきん <156275155+wakin721@users.noreply.github.com>
2026-09-07 19:52:11 +0800

    chore: bump Neri to 3.0.6-alpha3

--- system/version.py
+++ system/version.py
@@ -3,9 +3,9 @@ import re
 import subprocess
 from pathlib import Path
 
-BASE_VERSION = "3.0.6-alpha2"
+BASE_VERSION = "3.0.6-alpha3"
 DEFAULT_BUILD_CODE = "0f6ac7"
-DEFAULT_BUILD_NUMBER = 459
+DEFAULT_BUILD_NUMBER = 460
 
 _BUILD_CODE_PATTERN = re.compile(r"^[0-9a-fA-F]{6}$")
 _BUILD_NUMBER_PATTERN = re.compile(r"^\d{1,5}$")
